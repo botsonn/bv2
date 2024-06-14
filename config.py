1ef23fa574c9dcb23f2ecf348d66c41bec4246d1
botsonn <ahmdghyly09@gmail.com>
2024-06-14 17:16:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 21627756))
+    APP_ID = int(os.environ.get("APP_ID", 28094107))
 
-    API_HASH = os.environ.get("API_HASH", "fe77fbf0cae9f7f5ece37659e2466cf1")
+    API_HASH = os.environ.get("API_HASH", "1ff3955d2e53a2f76c1599b09e8f7dc2")
